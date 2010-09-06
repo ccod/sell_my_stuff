ccafff6b5c805b9717e8ef3fa454fb68a02960e8
Christopher Codrington <chcodrington@gmail.com>
2010-09-06 16:50:12 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SellMyStuff::Application.config.secret_token = '5f7adf823f8149220d03c67531a042b9c3870f65f3105768f2886b25190a1a9f63fdd72e6ae54c2a74204ff5ff288d483558266d5dcedc212c340a2e7b66985f'
